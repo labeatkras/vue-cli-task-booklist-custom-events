395d9cb75a250cc15a9c698549d9ab7e6ed06f7c
Labeat Krasniqi <labeat.kras@gmail.com>
2023-08-20 00:20:31 +0200

    vue-cli-task-booklist-custom-events

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import BaseButton from "@/components/BaseButton.vue";
 
-createApp(App).mount("#app");
+import "@/assets/reset.css";
+
+const app = createApp(App);
+
+app.component("BaseButton", BaseButton);
+
+app.mount("#app");
